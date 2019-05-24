4bedf30044fd0fa0378cabee176a0fb7f8cd2e35
thomas lombardo <tomlom1984@hotmail.co.uk>
2019-05-24 17:02:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var kittens = ["Milo", "Otis", "Garfield"]; //define your array here
-
+var name = "Ralph"
 // Add your functions and code here
 function destructivelyAppendKitten (name) {
-  return kittens.unshift("Ralph");
+  return kittens.unshift(name);
 }
