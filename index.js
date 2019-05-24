122ab745c7447c883e1d9b46bc93eb106a9a77c0
thomas lombardo <tomlom1984@hotmail.co.uk>
2019-05-24 17:04:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var kittens = ["Milo", "Otis", "Garfield"]; //define your array here
+var kittens = ["Milo", "Otis", "Garfield"];
 var name = "Ralph";
-// Add your functions and code here
+
 function destructivelyAppendKitten (name) {
   return kittens.push(name);
 }
