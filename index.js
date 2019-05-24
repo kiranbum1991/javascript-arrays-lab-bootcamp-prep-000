ab3d9d8e3b34258db866b64bec0930452f0c975a
thomas lombardo <tomlom1984@hotmail.co.uk>
2019-05-24 17:20:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,6 +21,8 @@ function appendKitten (name) {
   return kittens.concat(name);
 }
 
+var name = "Arnold";
+
 function prependKitten (name) {
   kittens.slice(0, 2);
   return kittens.unshift(name);
